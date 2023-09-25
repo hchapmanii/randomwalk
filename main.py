e7783c8fba673108e0cd5a00227725a29aad2066
Heyward Chapman <112347513+hchapmanii@users.noreply.github.com>
2023-09-25 12:58:18 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,21 @@
+from turtle import Turtle, Screen
+import random
+
+tim = Turtle()
+colours = ["purple", "red", "blue", "brown", "pink", "black", "yellow", "green"]
+angles = [90, 180, 360, 0]
+
+
+def moving():
+    tim.width(5)
+    tim.speed("fast")
+    tim.color(random.choice(colours))
+    tim.forward(50)
+    tim.setheading(random.choice(angles))
+
+
+for m in range(500):
+    moving()
+
+my_screen = Screen()
+my_screen.exitonclick()
