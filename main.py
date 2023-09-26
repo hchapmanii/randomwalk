8e71d9e97c775ab6d8d7897918afedaffcfc644e
Heyward Chapman <112347513+hchapmanii@users.noreply.github.com>
2023-09-25 22:14:07 -0400

    Update main.py
    Addition colors update

--- main.py
+++ main.py
@@ -1,20 +1,32 @@
+import turtle
 from turtle import Turtle, Screen
 import random
 
 tim = Turtle()
-colours = ["purple", "red", "blue", "brown", "pink", "black", "yellow", "green"]
-angles = [90, 180, 270, 0]
+turtle.colormode(255)
+angles = [90, 180, 360, 0]
+
+
+def colors():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    rgb = (r, g, b)
+    print(rgb)
+    return rgb
 
 
 def moving():
     tim.width(5)
     tim.speed("fast")
-    tim.color(random.choice(colours))
-    tim.forward(50)
+    # tim.color(random.choice(colours))
+    tim.color(colors())
+    tim.forward(30)
     tim.setheading(random.choice(angles))
 
 
 for m in range(500):
+    colors()
     moving()
 
 my_screen = Screen()
